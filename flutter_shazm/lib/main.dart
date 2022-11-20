5b72a3f15874efb2fec12a6433af16f84eb70504
KuroS2 <102028778+alfredsty@users.noreply.github.com>
2022-11-20 21:11:03 +0900

    2022/11/20

--- flutter_shazm/lib/main.dart
+++ flutter_shazm/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -119,7 +121,176 @@ class FirstTab extends StatelessWidget {
     ];
 
     return SafeArea(
-      child: Container(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Icon(Icons.settings),
+                ),
+                Text(
+                  '라이브러리',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(null),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                children: [
+                  ImageIcon(
+                    NetworkImage('https://i.ibb.co/hxNbZ8p/shazam.png'),
+                    size: 18,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Shazam',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(width: 10),
+                  Text(
+                    '아티스트',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                children: [
+                  Icon(Icons.music_note_sharp),
+                  SizedBox(width: 10),
+                  Text(
+                    '회원님을 위한 재생목록',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Text(
+              '최근 Shazam',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3 / 5,
+                ),
+                itemCount: songs.length,
+                itemBuilder: (context, index) {
+                  var song = songs[index];
+                  String imageUrl = song['imageUrl']!;
+                  String title = song['title']!;
+                  String artist = song['artist']!;
+
+                  index % 2 == 0; // 왼쪽, 1이면 오른쪽
+                  return Container(
+                    margin: EdgeInsets.only(
+                      left: 4,
+                      right: 4,
+                      top: 4,
+                      bottom: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 1,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(8),
+                            topRight: Radius.circular(8),
+                          ),
+                          child: Image.network(
+                            imageUrl,
+                            fit: BoxFit.cover,
+                            height: MediaQuery.of(context).size.width *
+                                0.5 *
+                                5 /
+                                3 *
+                                0.55,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            width: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  title,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  artist,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                Spacer(),
+                                Image.network(
+                                  "https://i.ibb.co/KG9m5QS/applemusic.png",
+                                  width: 60,
+                                ),
+                                SizedBox(height: 5),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
